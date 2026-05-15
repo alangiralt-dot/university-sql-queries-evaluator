747f2e0bd5aca1f24ed02de9c19b3b5ff970d265
alangiralt-dot <alangiralt@telefonica.net>
2026-05-15 09:36:07 +0200

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -71,7 +71,10 @@ LEFT JOIN asignatura a ON prof.id_profesor = a.id_profesor
 WHERE a.id_profesor IS NULL;
 
 -- 14. Retorna un llistat amb les assignatures que no tenen un professor/a assignat. (id, nombre)
-
+SELECT a.id, a.nombre 
+FROM asignatura a 
+LEFT JOIN profesor prof ON a.id_profesor = prof.id_profesor 
+WHERE prof.id_profesor IS NULL;
 
 -- 15. Retorna un llistat amb tots els departaments que no han impartit assignatures en cap curs escolar. (nombre)
 
